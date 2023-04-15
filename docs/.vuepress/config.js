e3dcf2030e630e0ce48ea79912ff5bf00beed604
mapNie <a0785660@163.com>
2023-04-15 21:40:57 +0800

    增加了git和vue3，并修改了github地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,7 @@ module.exports = {
         nav: [
 			{ text: "联系作者", link: "/guide/concact" }, // link是路径
 			// link 可以链接外部地址，界面样式会带有 小箭头 
-			{ text: "github", link: "https://github.com/mapNie/Front-end-sharing" },
+			{ text: "github", link: "https://github.com/mapNie/Front-end-sharing.git" },
 		],
 		// 左侧导航
         sidebar: 
@@ -20,11 +20,13 @@ module.exports = {
               'JavaScrip',
               'Css-3',
               'Vue-2',
+              'Vue-3',
               'TypeScript',
               'UniApp',
               'Flutter',
               'MiniProgram',
               'InterView',
+              'Git',
             ]
         ,
 	},
