19d85c6f51ac15cbcb3ebf08ce92416bba251fe0
mapNie <a0785660@163.com>
2023-04-15 21:26:08 +0800

    前端知识分享

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    locales: {
+        '/': {
+          lang: 'zh-CN'
+        },
+    },
+    hero:'/assets/img/home.jpeg',
+    title: '前端开发',
+    description: '欢迎大家一起参考与学习',
+    themeConfig: {
+        logo: '/assets/img/logo.jpeg',
+        nav: [
+			{ text: "联系作者", link: "/guide/concact" }, // link是路径
+			// link 可以链接外部地址，界面样式会带有 小箭头 
+			{ text: "github", link: "https://github.com/mapNie/Front-end-sharing" },
+		],
+		// 左侧导航
+        sidebar: 
+            [
+              'JavaScrip',
+              'Css-3',
+              'Vue-2',
+              'TypeScript',
+              'UniApp',
+              'Flutter',
+              'MiniProgram',
+              'InterView',
+            ]
+        ,
+	},
+	lastUpdated: 'Last Updated',  // 最后更新时间
+    }
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroText: 分享各种前端知识
+tagline: 一起学习
+actionText: 进入文档 →
+actionLink: /JavaScrip/
+features:
+- title: JavaScript
+  details: 记录前端js相关内容
+- title: Css
+  details: 记录css相关内容
+- title: 八股文
+  details: 记录常见的八股文和算法
+footer: 个人所有
+---
